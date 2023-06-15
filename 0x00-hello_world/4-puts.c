55d1f12d312888dfa0a436226815ec8ba2e56dc5
BuzzyBrain <ricardnkrumah760@gmail.com>
2023-06-15 01:38:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0
+ * main - A program that prints a line with puts functions*
+  * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
